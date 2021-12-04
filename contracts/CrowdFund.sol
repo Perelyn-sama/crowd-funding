dd69214328dddfc6f4803b091d99c2fbc5b33eb5
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2021-12-05 00:37:10 +0100

    setVaribles

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+// Declaring contract named CrowdFunding
+contract CrowdFunding {
+    // Declaring state varibles 
+    uint amount;
+    uint goal;
+
+    function setVaribles (uint _amount, uint _goal ) public view {
+        _amount = amount;
+        _goal = goal;
+    }
+}
